63e19c49d97b56586fcc598d692c153519ad511f
DdZz2005 <daniilka.zinchenko.05@mail.ru>
2024-09-03 23:13:00 +0300

    Последняя рабочка

--- app/src/main/java/com/example/craft_code_mobile_project/ScanActivity.kt
+++ app/src/main/java/com/example/craft_code_mobile_project/ScanActivity.kt
@@ -1,6 +1,9 @@
 package com.example.craft_code_mobile_project
 
+import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.journeyapps.barcodescanner.CompoundBarcodeView
 import com.journeyapps.barcodescanner.BarcodeCallback
@@ -10,31 +13,37 @@ class ScanActivity : AppCompatActivity() {
 
     private lateinit var barcodeView: CompoundBarcodeView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scan)  // Убедитесь, что путь к вашему XML правильный
-
-        // Инициализация CompoundBarcodeView
-        barcodeView = findViewById(R.id.barcode_scanner)
-
-
-        // Запуск непрерывного сканирования
-        barcodeView.decodeContinuous(callback)
-    }
-
     // Callback для обработки результатов сканирования
     private val callback = object : BarcodeCallback {
         override fun barcodeResult(result: BarcodeResult?) {
             result?.let {
-                // Обработка результата сканирования
                 val qrCodeContent = result.text
-                // Действия после сканирования, например, показать уведомление или перейти на другую активность
+                // Переход к следующей активности с результатом сканирования
+                val intent = Intent(this@ScanActivity, ActionWhithTheQrActivity::class.java).apply {
+                    putExtra("QR_CODE", qrCodeContent)
+                }
+                startActivity(intent)
+                finish() // Завершаем текущую активность после сканирования
             }
         }
 
         override fun possibleResultPoints(resultPoints: List<com.google.zxing.ResultPoint>) {
             // Необязательная обработка возможных точек результата
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan) // Убедитесь, что путь к вашему XML правильный
+
+        // Принудительная установка ориентации экрана в портретную
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        // Инициализация CompoundBarcodeView
+        barcodeView = findViewById(R.id.barcode_scanner)
+
+        // Запуск непрерывного сканирования
+        barcodeView.decodeContinuous(callback)
     }
 
     override fun onResume() {
