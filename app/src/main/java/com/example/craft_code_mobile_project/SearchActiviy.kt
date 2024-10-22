4b02701079aed1d0620a2a132130a87e5940c591
DdZz2005 <daniilka.zinchenko.05@mail.ru>
2024-10-22 20:10:20 +0300

    Последня рабочая версия!

--- app/src/main/java/com/example/craft_code_mobile_project/SearchActiviy.kt
+++ app/src/main/java/com/example/craft_code_mobile_project/SearchActiviy.kt
@@ -1,9 +1,13 @@
 package com.example.craft_code_mobile_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.craft_code_mobile_project.databinding.ActivitySearchBinding
+import utils.CameraFunctions
 
 class SearchActivity : AppCompatActivity() {
 
@@ -32,6 +36,45 @@ class SearchActivity : AppCompatActivity() {
             // Установка LayoutParams для равномерного распределения
             val params = Toolbar.LayoutParams(0, Toolbar.LayoutParams.MATCH_PARENT)
             itemView.layoutParams = params
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.account_details -> {
+                val intent = Intent(this, AccountDetailsActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.company_details -> {
+                val intent = Intent(this, CompanyDetailsActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.action_document -> {
+                // Действие при нажатии на иконку документа
+                true
+            }
+            R.id.action_camera -> {
+                if (CameraFunctions.checkCameraPermission(this)) {
+                    val intent = Intent(this, ScanActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    CameraFunctions.requestCameraPermission(this)
+                }
+                true
+            }
+            R.id.action_search -> {
+                val intent = Intent(this, SearchActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
